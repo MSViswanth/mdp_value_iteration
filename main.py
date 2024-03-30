8f84c836c4808c97b5e8f32a9abeb9500a2fadf8
Sai Viswanth Mullapudi <viswanth.mullapudi28@gmail.com>
2024-03-29 22:19:44 -0400

    Add mdp object

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import copy
-import math
 
 
 rs = [-100, -3, 0, 3]
 r = -100
-action_space = ["l", "u", "r", "d"]
 discount_factor = 0.99
 
 
-grid_world_rewards = [[r, -1, 10], [-1, -1, -1], [-1, -1, -1]]
-# class MDPObject:
+class MDPObject:
+    def __init__(self, m, n):
+        self.states = []
+        self.rewards = []
+        for i in range(0, m):
+            self.rewards.append([])
+            for j in range(0, n):
+                self.states.append((i, j))
+                self.rewards[i].append(-0.04)
+        self.actions = ["l", "u", "r", "d"]
+        self.states.remove((1, 1))
+        self.rewards[3][2] = +1
+        self.rewards[3][1] = -1
+        # self.gamma = 0.99
+        self.gamma = 1
+        # self.rewards = [[r, -1, 10], [-1, -1, -1], [-1, -1, -1]]
 
 
 def q_value(mdp, s, a, U):
+    # print(mdp.actions.index(a) % 4 + 1)
     possible_actions = [
-        mdp.actions[mdp.actions.index(a) % 4 + 1],
-        mdp.actions[mdp.actions.index(a) % 4 - 1],
-        a
+        mdp.actions[(mdp.actions.index(a) + 1) % 4],
+        mdp.actions[(mdp.actions.index(a) - 1) % 4],
+        a,
     ]
+    r = mdp.rewards
     q_value = 0
     for action in possible_actions:
         if action == a:
@@ -25,33 +39,43 @@ def q_value(mdp, s, a, U):
         else:
             p = 0.1
         match action:
-            case 'l':
-                s_ = (s[0]-1, s[1])
-            case 'r':
-                s_ = (s[0]+1, s[1])
-            case 'u':
-                s_ = (s[0], s[1]+1)
-            case 'd':
-                s_ = (s[0], s[1]-1)
-        q_value += p * (r[*s_] + mdp.gamma * U[*s_])
+            case "l":
+                s_ = (s[0] - 1, s[1])
+            case "r":
+                s_ = (s[0] + 1, s[1])
+            case "u":
+                s_ = (s[0], s[1] + 1)
+            case "d":
+                s_ = (s[0], s[1] - 1)
+        if s_ not in mdp.states:
+            s_ = s
+        q_value += p * (r[s_[0]][s_[1]] + mdp.gamma * U[s_])
+    return q_value
 
 
 def value_iteration(mdp, e):
     S = mdp.states
     A = mdp.actions
     U = {}
-    U.keys = S
     U = {s: 0 for s in S}
     U_ = copy.deepcopy(U)
     gamma = mdp.gamma
 
     while True:
-        U = U_
+        # print('in while')
+        U = copy.deepcopy(U_)
         delta = 0
         for s in S:
             U_[s] = max([q_value(mdp, s, a, U) for a in A])
-            if math.abs(U_[s] - U[s]) > delta:
-                delta = math.abs(U_[s] - U[s])
+            # print(U_[s])
+            if (U_[s] - U[s]) > delta:
+                delta = (U_[s] - U[s])
+        print(delta)
         if delta <= e * ((1 - gamma) / gamma):
             break
     return U
+
+
+mdp = MDPObject(4, 3)
+u = value_iteration(mdp, 0.0000000001)
+print(u)
